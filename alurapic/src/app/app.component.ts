9b13438392f5cdb630c37fa00088098e6f3acbd9
Jonas <40861129+jnsbatista@users.noreply.github.com>
2021-03-09 23:13:34 -0300

    curso alura 09/03/21 depois das 22h

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://i.pinimg.com/originals/45/91/c8/4591c8db6cb652a3e73cc8595bcf40b8.jpg',
+      description: 'Leoa'
+    }
+  ];
+
 }
