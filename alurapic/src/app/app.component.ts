cd9a97982500f9e29041e7a49058e7ad5fd66471
Jonas <40861129+jnsbatista@users.noreply.github.com>
2021-03-10 14:50:15 -0300

    10/03 manhã

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    },
-    {
-      url: 'https://i.pinimg.com/originals/45/91/c8/4591c8db6cb652a3e73cc8595bcf40b8.jpg',
-      description: 'Leoa'
-    }
-  ];
+  title = 'Welcome to'
+  photos: any[] = []
+// private ou public
+  constructor(private photoService: PhotoService){
+
+  }
+
+  ngOnInit(): void {
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => {
+        this.photos = photos
+    })
+  }
 
 }
