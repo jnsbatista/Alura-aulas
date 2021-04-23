9b047e11ef86dee73473ddc988460566e27aaf83
Jonas <jnsbatista2009@gmail.com>
2021-04-23 09:14:00 -0300

    c

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-
-import { PhotoModule } from './photo/photo-module';
+import { PhotoModule } from './photo/photo.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
+import { CardModule } from '../shared/components/card/card.module';
+import { DarkenOnHoverModule } from '../shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
-
-    imports: [
+    imports: [ 
         PhotoModule,
         PhotoFormModule,
         PhotoListModule,
-        HttpClientModule,
-        CommonModule
+        DarkenOnHoverModule
     ]
 })
 export class PhotosModule {}
