b3e1e7793894a790bbe95a055d1c28d876385962
Jonas <jnsbatista2009@gmail.com>
2021-05-06 11:54:43 -0300

    servicos de busca

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "../user/user";
+import { UserService } from "../user/user.service";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  user$: Observable<User>; //$ é uma boa prática para observable
+  user: User;
+
+  constructor(userService: UserService) {
+    this.user$ = userService.getUser();
+    this.user$.subscribe((user) => (this.user = user)); //subscribe() é usado para poder ler o objeto em header.componente.html
+  }
+}
